df16cc4cf91ce33a6de5e6e04c3dd36a0803120d
Harry Ross <hazross@hotmail.com>
2021-02-27 09:25:37 +1100

    got a todo thing but not really functional yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Grommet, Nav, Anchor, Main, FormField, TextInput, Form, CheckBox } from 'grommet';
+import { grommet } from 'grommet/themes'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = { 
+        todoInput: "",
+        todos: []
+     }
+    componentDidMount() {
+        let todoDescs = localStorage.getItem('todos').split(';');
+        let todos = todoDescs.map(todoDesc => {return({desc: todoDesc, checked: false})} )
+        this.setState({
+            todos
+        })
+    }
+    render() { 
+        return ( 
+            <div className="App">
+                <Grommet theme={grommet}>
+                    <Nav background="dark-1" pad="medium">
+                        <Anchor>yo</Anchor>
+                    </Nav>
+                    <Main>
+                        <Form
+                            onSubmit={() => {
+                                console.log(this.state.todoInput)
+                                var localTodos = localStorage.getItem('todos')
+                                localStorage.setItem('todos', `${localTodos};${this.state.todoInput}`)
+                                this.setState({
+                                    todoInput: "",
+                                })
+                            }}
+                        >
+                            <FormField>
+                                <TextInput 
+                                    value={this.state.todoInput}
+                                    placeholder="Todo here"
+                                    onChange={(event)=> {
+                                        this.setState({ todoInput: event.target.value })
+                                    }} 
+                                />
+                            </FormField>
+                        </Form>
+                        {this.state.todos.length > 0
+                            ? this.state.todos.map(todo => { return(<CheckBox checked={false} label={todo}></CheckBox>)})
+                            : <p>empty</p>
+                        }
+                    </Main>
+                </Grommet>
+            </div>
+        );
+    }
 }
-
+ 
 export default App;
