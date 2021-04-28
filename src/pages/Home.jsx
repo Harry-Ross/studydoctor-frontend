b4cfeacb5b790b51b0bab58e9d6f20c6b181e46d
Harry Ross <hazross@hotmail.com>
2021-04-28 14:41:24 +1000

    changing my stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,17 @@ import axios from 'axios';
 
 class HomePage extends Component {
     state = {  }
+    componentDidMount() {
+        axios.get('http://localhost:4000/api/classroom/tasks')
+    }
+
     render() { 
         return (
             <div>
-                <GoogleLogin
-                    clientId={"304421778393-q3l61amqah3oiqs73aoss91glhofds0o.apps.googleusercontent.com"}
-                    buttonText="Log in with Google"
-                    onSuccess={handleLogin}
-                    onFailure={(err) => { console.error(err.details) }}
-                    isSignedIn={true}
-                />
+                
             </div>
         );
     }
-}
-
-const handleLogin = (googleData) => {
-    console.log(googleData)
-    axios.post(`http://localhost:4000/api/auth/google`, { accessToken: googleData.accessToken, idToken: googleData.tokenId })
-        .then(res => {
-            console.log(res.data)
-        })
 }
  
 export default HomePage;
