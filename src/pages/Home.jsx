4d398b301c855685b1569c07e2ffa5867e00243d
Harry Ross <hazross@hotmail.com>
2021-05-01 14:35:04 +1000

    added features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,22 @@
 import React, { Component } from 'react'
 import GoogleLogin from 'react-google-login'
 import axios from 'axios';
+import classroomService from '../services/classroom-service';
 
 class HomePage extends Component {
+    componentDidMount() {
+        classroomService.getClasses().then(data => {
+            console.log(data)
+        })
+    }
     state = {  }
     render() { 
         return (
             <div>
-                <GoogleLogin
-                    clientId={"304421778393-q3l61amqah3oiqs73aoss91glhofds0o.apps.googleusercontent.com"}
-                    buttonText="Log in with Google"
-                    onSuccess={handleLogin}
-                    onFailure={(err) => { console.error(err.details) }}
-                    isSignedIn={true}
-                />
+                <p>yeahhh</p>
             </div>
         );
     }
-}
-
-const handleLogin = (googleData) => {
-    console.log(googleData)
-    axios.post(`http://localhost:4000/api/auth/google`, { accessToken: googleData.accessToken, idToken: googleData.tokenId })
-        .then(res => {
-            console.log(res.data)
-        })
 }
  
 export default HomePage;
